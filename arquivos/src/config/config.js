8f39863214a3c34eab41aa4c5a7943c22040bb9b
Cleiton Estefenon <cleitonestefenon@gmail.com>
2019-08-24 11:35:55 -0300

    add arquivo de exemplo de conexao co o mongo

---
+++ arquivos/src/config/config.js
@@ -0,0 +1,7 @@
+const databaseConnection = "mongodb+srv://clediano:clediano@cluster0-1uusa.mongodb.net/test?retryWrites=true&w=majority";
+const authSecret = "0822df7867416c1cc7a5d5b2eea110df";
+
+module.exports = {
+    databaseConnection,
+    authSecret
+}
