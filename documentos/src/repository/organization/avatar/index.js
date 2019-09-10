0f6ce9d982c31482dae6898902ea6410974aebb1
clediano <cledianoestefenon@gmail.com>
2019-09-09 21:22:03 -0300

    Finalizada funcionalidade para atualizar o avatar com qualquer tipo de imagem.

--- documentos/src/repository/organization/avatar/index.js
+++ documentos/src/repository/organization/avatar/index.js
@@ -9,75 +9,41 @@ async function updateAvatar(req, res) {
 
     if (!organization) return res.json({ error: 'Organização não encontrada.' });
 
+    const formData = new FormData();
+    formData.append('file', fs.createReadStream(req.file.path));
+    formData.append('filename', req.file.filename);
+    formData.append('mimetype', req.file.mimetype);
+    formData.append('size', req.file.size);
+
+    const config = { headers: { 'Content-Type': `multipart/form-data; boundary=${formData.getBoundary()}` } };
+
     if (organization.oidphoto) {
-        axios.delete(`/file/${organization.oidphoto}`)
-            .then(() => {
-                
-                console.log(req.file)
-                fs.readFile(req.file.path, (err, file) => {
+        axios.delete(`/file/${organization.oidphoto}`).catch(err => {
+            return res.json({ error: 'Ocorreu um erro ao atualizar o avatar. Tente novamente mais tarde.' });
+        })
+        saveImage(req, res, formData, config);
 
-                    if (err) return res.json({ error: 'Erro ao ler a imagem, por favor, tente novamente.' });
-
-                    const formData = new FormData();
-                    formData.append('file', fs.createReadStream(req.file.path));
-                    const config = { headers: { 'Content-Type': `multipart/form-data; boundary=${formData.getBoundary()}` } };
-
-                    axios.post('/file', formData, config)
-                        .then(resp => {
-
-                            fs.unlink(req.file.path, err => {
-                                if (err) console.error('Erro ao deletar a imagem.', err);
-                            });
-
-                            Organization.update({ oidphoto: resp.data._id }, { where: { id: req.params.id } });
-
-                            return res.status(201).json({
-                                _id: resp.data._id,
-                                hash: resp.data.hash
-                            });
-
-                        })
-                        .catch(err => {
-                            console.error(err)
-                            return res.status(400).json({ error: 'Erro ao salvar arquivo no banco de dados.' })
-                        })
-                });
-
-            })
-            .catch(err => {
-                return res.json({ error: 'Ocorreu um erro ao atualizar o avatar. Tente novamente mais tarde.' });
-            })
     } else {
-        fs.readFile(req.file.path, (err, file) => {
-
-            if (err) return res.json({ error: 'Erro ao ler a imagem, por favor, tente novamente.' });
-
-            const formData = new FormData();
-            formData.append('file', fs.createReadStream(req.file.path));
-
-            const config = { headers: { 'Content-Type': `multipart/form-data; boundary=${formData.getBoundary()}` } };
-
-            axios.post('/file', formData, config)
-                .then(resp => {
-
-                    fs.unlink(req.file.path, err => {
-                        if (err) console.error('Erro ao deletar a imagem.', err);
-                    });
-
-                    Organization.update({ oidphoto: resp.data._id }, { where: { id: req.params.id } });
-
-                    return res.status(201).json({
-                        _id: resp.data._id,
-                        hash: resp.data.hash
-                    });
-
-                })
-                .catch(err => {
-                    console.error(err)
-                    return res.status(400).json({ error: 'Erro ao salvar arquivo no banco de dados.' })
-                })
-        });
+        saveImage(req, res, formData, config);
     }
+}
+
+async function saveImage(req, res, formData, config) {
+    axios.post('/file', formData, config)
+        .then(({ data }) => {
+            Organization.update({ oidphoto: data._id }, { where: { id: req.params.id } });
+
+            return res.status(201).json(data);
+        })
+        .catch(err => {
+            console.error(err)
+            return res.status(400).json({ error: 'Erro ao salvar arquivo no banco de dados.' })
+        })
+        .finally(() => {
+            fs.unlink(req.file.path, err => {
+                if (err) console.error('Erro ao deletar a imagem.', err);
+            });
+        })
 }
 
 module.exports = {
